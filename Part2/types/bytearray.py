07a8b83355e4dcf067fd3f97157327ba255692d3
Drus <zxandruxazx56@yandex.ru>
2025-09-01 23:31:41 +0500

    ch

---
+++ Part2/types/bytearray.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+S = 'shrubbery'
+L = list(S)
+print(L)
+L[1] = 'c'
+print(''.join(L))
+
+B = bytearray(b'spam')
+B.extend(b'eggs')
+print(B)
+print(B.decode())
---
+++ Part2/types/spec_symbols.py
@@ -0,0 +1,13 @@
+S = 'A\nB\tC'
+print(len(S))
+print(ord('\n'))
+S = 'A\0B\0C'
+print(len(S))
+print(S)
+
+msg = """
+aaaaaaaaa
+bbbb''''''bbbbbbbb""bb'bbb
+ccccccc
+"""
+print(msg)
